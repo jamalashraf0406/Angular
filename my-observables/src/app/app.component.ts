ac90eb431f921eb4fbf8f302b418e9b6b72161ba
Jamal Ashraf <jamal.ashraf_ct@SW-LP10595.local>
2021-05-26 15:23:57 +0530

    updates

--- my-observables/src/app/app.component.ts
+++ my-observables/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from "./user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-observables';
+export class AppComponent implements OnInit, OnDestroy {
+  userActivate: boolean = false;
+  private activeSub: Subscription;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.activeSub = this.userService.activatedSubject
+      .subscribe((data: boolean) => {
+        this.userActivate = data;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.activeSub.unsubscribe();
+  }
 }
