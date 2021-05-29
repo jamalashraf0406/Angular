af8059da1a80b4f1574c335e3d4beff240a5f7c8
Jamal Ashraf <jamal.ashraf_ct@SW-LP10595.local>
2021-05-29 12:00:57 +0530

    updates

--- my-first-app/src/app/recipes/recipe.service.ts
+++ my-first-app/src/app/recipes/recipe.service.ts
@@ -1,42 +1,44 @@
-import { Injectable} from "@angular/core";
+import {Injectable, OnInit} from "@angular/core";
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
 import {Subject} from "rxjs";
 
 @Injectable()
-export class RecipeService {
+export class RecipeService implements OnInit{
 
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "Simply test recipe",
-      "https://www.forksoverknives.com/wp-content/uploads/vegan-quesadilla-recipe-quick-easy-plant-based-recipes.jpg",
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
+  // private recipes: Recipe[] = [
+  //   new Recipe("A Test Recipe", "Simply test recipe",
+  //     "https://www.forksoverknives.com/wp-content/uploads/vegan-quesadilla-recipe-quick-easy-plant-based-recipes.jpg",
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //
+  //   new Recipe("Another Recipe", "Simply test recipe",
+  //     "https://ca-times.brightspotcdn.com/dims4/default/78f26f2/2147483647/strip/true/crop/4032x2688+0+168/resize/840x560!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Ffc%2Ffe%2Fee3e952643658869005722ed0dd7%2Fla-photos-1staff-693155-la-fo-oven-baked-nachos.jpg",
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //
+  //   new Recipe("Chicken Recipe", "Simply test recipe",
+  //     "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/recipes/tangy_skillet_chicken_recipe/650x350_tangy_skillet_chicken_recipe.jpg",
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Check fry', 20)
+  //     ]),
+  //   new Recipe("Chicken Recipe", "Simply test recipe",
+  //     "https://cdn.shopify.com/s/files/1/1717/1391/articles/Huli_Huli_Chicken_1112x.jpg?v=1486952708",
+  // [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Check fry', 20)
+  //   ])
+  // ];
 
-    new Recipe("Another Recipe", "Simply test recipe",
-      "https://ca-times.brightspotcdn.com/dims4/default/78f26f2/2147483647/strip/true/crop/4032x2688+0+168/resize/840x560!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Ffc%2Ffe%2Fee3e952643658869005722ed0dd7%2Fla-photos-1staff-693155-la-fo-oven-baked-nachos.jpg",
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-
-    new Recipe("Chicken Recipe", "Simply test recipe",
-      "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/recipes/tangy_skillet_chicken_recipe/650x350_tangy_skillet_chicken_recipe.jpg",
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Check fry', 20)
-      ]),
-    new Recipe("Chicken Recipe", "Simply test recipe",
-      "https://cdn.shopify.com/s/files/1/1717/1391/articles/Huli_Huli_Chicken_1112x.jpg?v=1486952708",
-  [
-        new Ingredient('Meat', 1),
-        new Ingredient('Check fry', 20)
-])
-];
+  recipes: Recipe[] = []
 
   constructor(private slService: ShoppingListService) {
   }
@@ -66,5 +68,14 @@ export class RecipeService {
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
     this.recipeChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  ngOnInit(): void {
+
   }
 }
