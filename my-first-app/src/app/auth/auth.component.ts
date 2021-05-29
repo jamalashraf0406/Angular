0fca30549d05daa69b256a2fb417a22883314072
Jamal Ashraf <jamal.ashraf_ct@SW-LP10595.local>
2021-05-29 16:54:52 +0530

    updates

---
+++ my-first-app/src/app/auth/auth.component.ts
@@ -0,0 +1,56 @@
+import {Component} from "@angular/core";
+import {NgForm} from "@angular/forms";
+import {AuthResponseData, AuthService} from "./auth.service";
+import {Observable} from "rxjs";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
+})
+export class AuthComponent {
+
+  isLoginMode: boolean = true;
+  isLoading: boolean = false;
+  error: string = null
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    if (!authForm.valid) {
+      return;
+    }
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+    this.isLoading = true;
+
+    if ( this.isLoginMode ) {
+     authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
+    }
+
+
+    authObs.subscribe(response => {
+        console.log(response);
+        this.isLoading = false;
+        this.router.navigate(['/recipes'])
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    authForm.reset();
+  }
+}
--- my-first-app/src/app/header/header.component.ts
+++ my-first-app/src/app/header/header.component.ts
@@ -1,13 +1,19 @@
-import { Component } from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import {DataStorageService} from "../shared/data-storage.service";
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-header',
     templateUrl:'./header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements  OnInit, OnDestroy {
 
-  constructor(private dataStorageService: DataStorageService) {
+  isAuthenticated: boolean = false;
+  private userSub: Subscription;
+
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) {
   }
 
   onSaveData() {
@@ -16,5 +22,22 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.userSubject.subscribe(
+      user => {
+          this.isAuthenticated = !!user;
+          console.log(!user);
+          console.log(!!user);
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
